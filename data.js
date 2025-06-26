ba97f154ac01c912d68432919729e79689b359b0
Mahigautam27 <mahigautam0052@gmail.com>
2025-06-26 12:41:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image: 'https://images.unsplash.com/photo-1694069010624-62456fd0459c?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image: 'https://images.unsplash.com/photo-1727895468642-d9f1660c5401?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image: 'https://images.unsplash.com/photo-1694003260394-f83cf50c93a3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image: 'https://images.unsplash.com/photo-1682786308110-fab1fe5e9446?q=80&w=584&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image: 'https://images.unsplash.com/photo-1680096283038-eaeb2f532cbc?q=80&w=397&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
